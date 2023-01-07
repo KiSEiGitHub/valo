21cb0356313fe98966888b9fd1685f51993c18b4
KiSEiGitHub <tommusique974@gmail.com>
2023-01-07 22:39:44 +0100

    :feat: head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import Nav from "../components/Nav";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
+      <Head>
+        <title>Valorant</title>
+      </Head>
       <Nav />
       <Component {...pageProps} />
     </ChakraProvider>
