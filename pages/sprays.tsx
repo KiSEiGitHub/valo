4ca1611a71f1084d4cdbb2940cdce85b15f0182a
KiSEiGitHub <tommusique974@gmail.com>
2023-01-07 22:32:12 +0100

    :feat: spray

--- pages/sprays.tsx
+++ pages/sprays.tsx
@@ -1,9 +1,36 @@
-const Sprays = () => {
-    return (
-        <>
-            Sprays
-        </>
-    )
-}
+import { Flex, Image, Tooltip } from "@chakra-ui/react";
+import { GetServerSideProps } from "next";
 
-export default Sprays
+const Sprays = ({ sprays }: any) => {
+  console.log(sprays);
+
+  return (
+    <Flex flexWrap="wrap" gap={10} justifyContent="center" pt={50}>
+      {sprays.map((item: any, key: any) => (
+        <Tooltip key={key} label={item.displayName} borderRadius='md'>
+          <Image
+            src={item.fullTransparentIcon}
+            alt="ok"
+            w="120px"
+            h="120px"
+            objectFit="cover"
+            borderRadius="xl"
+          />
+        </Tooltip>
+      ))}
+    </Flex>
+  );
+};
+
+export default Sprays;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const res = await fetch("https://valorant-api.com/v1/sprays");
+  const { data } = await res.json();
+
+  return {
+    props: {
+      sprays: data,
+    },
+  };
+};
